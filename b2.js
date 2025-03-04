01c5701d54d71cccc9e49a6af4f84658de3a1051
hahuydz <haquanghuy2006abc@gmail.com>
2025-03-05 00:00:55 +0700

    b2.js

--- b2.js
+++ b2.js
@@ -1,13 +1,12 @@
-let N = prompt("Nhập một số nguyên dương: ");
-if (!isNaN(N) && Number.isInteger(Number(N)) && Number(N) > 0) {
-    N = Number(N);
-    let result = [];
+let N = +prompt("Nhập vào số nguyên N: ");
+
+if (N > 0) {
+    document.write("Các số chia hết cho 5 từ 1 đến " + N + " là: <br>");
     for (let i = 1; i <= N; i++) {
-        if (i % 5 === 0) {
-            result.push(i);
+        if (i % 5 == 0) {
+            document.write(i + " ");
         }
     }
-    console.log(`các số chia hết cho 5 từ 1 đến ${N} là : ${result.join(", ")}`);
 } else {
-    console.log("dữ liệu nhập vào không hợp lệ");
+    alert("Vui lòng nhập số nguyên dương!");
 }
